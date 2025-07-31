dbcd9b30c8017f61ffdcf67a13ab37744f865127
Mikolaj <boguszm2003@gmail.com>
2025-07-31 15:08:31 +0200

    added swagger documentation and README file

---
+++ README.md
@@ -0,0 +1,19 @@
+##Description
+
+Smiple REST API application for managing base of films
+
+##Requirements
+
+- Java 21
+- Maven
+- MySQL 8.0
+- MySql 8.0
+
+##Features
+- Create, Read, Update, Delete (CRUD) operations for films
+- Swagger documentation for API endpoints
+- Grading system for films
+
+
+##Documentation
+http://localhost:8080/swagger-ui/index.html#/
--- src/main/java/com/tcode/moviebase/Controllers/MovieController.java
+++ src/main/java/com/tcode/moviebase/Controllers/MovieController.java
@@ -4,6 +4,7 @@ import com.tcode.moviebase.Entities.Movie;
 import com.tcode.moviebase.Entities.MovieGrade;
 import com.tcode.moviebase.Repositories.MovieRepository;
 import com.tcode.moviebase.Services.MovieService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +18,7 @@ public class MovieController {
     private final MovieService movieService;
     private final MovieRepository movieRepository;
 
+    @Operation(summary = "Get all movies", description = "Retrieves a list of all movies in the database.")
     @GetMapping
     public ResponseEntity<List<Movie>> getAllMovies() {
         var movies = movieService.getAllMovies();
@@ -26,6 +28,7 @@ public class MovieController {
         return ResponseEntity.ok(movies);
     }
 
+    @Operation(summary = "Add a new movie", description = "Adds a new movie to the database.")
     @PostMapping
     public ResponseEntity<Movie> addMovie(@RequestBody Movie movie) {
         var movieAdded = movieService.addMovie(movie);
@@ -33,6 +36,7 @@ public class MovieController {
     }
 
 
+    @Operation(summary = "Delete a movie", description = "Deletes a movie by its ID from the database.")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteMovie(@PathVariable Long id) {
         var exists = movieService.getMovieById(id);
@@ -43,6 +47,7 @@ public class MovieController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(summary = "Get a movie by ID", description = "Retrieves a movie by its ID from the database.")
     @GetMapping("/{id}")
     public ResponseEntity<Movie> getMovie(@PathVariable Long id) {
         var movie = movieService.getMovieById(id);
@@ -52,6 +57,8 @@ public class MovieController {
         return ResponseEntity.ok(movie);
     }
 
+
+    @Operation(summary = "Get movies by category", description = "Retrieves a list of movies by their category.")
     @GetMapping("/findByCategory/{category}")
     public ResponseEntity<List<Movie>> getMoviesByCategory(@PathVariable String category) {
         var movies = movieService.findByCategory(category);
@@ -62,6 +69,7 @@ public class MovieController {
         }
     }
 
+    @Operation(summary = "Search movies by title", description = "Searches for movies by their title.")
     @GetMapping("/findByTitle")
     public ResponseEntity<List<Movie>> getMoviesByTitle(@RequestParam String title) {
         var movies = movieService.search(title);
@@ -72,6 +80,7 @@ public class MovieController {
         }
     }
 
+    @Operation(summary = "Add a grade to a movie", description = "Adds a grade to a movie by movie ID.")
     @PostMapping("/{id}/grade")
     public ResponseEntity<MovieGrade> addMovieGrade(@PathVariable Long id, @RequestParam int grade) {
         var movieGrade = movieService.addGrade(id, grade);
@@ -82,6 +91,7 @@ public class MovieController {
         }
     }
 
+    @Operation(summary = "Update a movie", description = "Updates an existing movie by its ID.")
     @PutMapping("/{id}")
     public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody Movie movie) {
         if (!movieRepository.existsById(id)) {
